1aee4d823f3825f587c2145e3ff197655f51f760
Smruthie Ramesh <70722786+smruthie@users.noreply.github.com>
2022-03-19 10:11:36 -0500

    Update Protein Sequence Content.py

--- Protein Sequence Content.py
+++ Protein Sequence Content.py
@@ -7,25 +7,7 @@ nonpolar_AA = 0
 sequence_list = list(peptide_sequence.upper())
 if 'G' in sequence_list:
     G = sequence_list.count('G')
-    nonpolar_AA += G
-if 'C' in sequence_list:
-    C = sequence_list.count('C')
-    nonpolar_AA += C
-if 'S' in sequence_list:
-    S = sequence_list.count('S')
-    polar_AA += S
-if 'T' in sequence_list:
-    T = sequence_list.count('T')
-    polar_AA += T
-if 'Y' in sequence_list:
-    Y = sequence_list.count('Y')
-    polar_AA += Y
-if 'N' in sequence_list:
-    N = sequence_list.count('N')
-    polar_AA += N
-if 'Q' in sequence_list:
-    Q = sequence_list.count('Q')
-    polar_AA += Q
+    hydrophobic_AA += G
 if 'D' in sequence_list:
     D = sequence_list.count('D')
     acidic_AA += D
@@ -44,35 +26,27 @@ if 'H' in sequence_list:
 if 'A' in sequence_list:
     A = sequence_list.count('A')
     hydrophobic_AA += A
-    nonpolar_AA += A
 if 'V' in sequence_list:
     V = sequence_list.count('V')
     hydrophobic_AA += V
-    nonpolar_AA += V
 if 'L' in sequence_list:
     L = sequence_list.count('L')
     hydrophobic_AA += L
-    nonpolar_AA += L
 if 'I' in sequence_list:
     I = sequence_list.count('I')
     hydrophobic_AA += I
-    nonpolar_AA += I
 if 'P' in sequence_list:
     P = sequence_list.count('P')
     hydrophobic_AA += P
-    nonpolar_AA += P
 if 'F' in sequence_list:
     F = sequence_list.count('F')
     hydrophobic_AA += F
-    nonpolar_AA += F
 if 'W' in sequence_list:
     W = sequence_list.count('W')
     hydrophobic_AA += W
-    nonpolar_AA += W
 if 'M' in sequence_list:
     M = sequence_list.count('M')
     hydrophobic_AA += M
-    nonpolar_AA += M
 hydrophobicity = hydrophobic_AA/len(sequence_list)*100
 if 'B' in sequence_list or 'O' in sequence_list or 'J' in sequence_list or 'X' in sequence_list or 'Z' in sequence_list or 'U' in sequence_list:
     print("Imposter(s) Alert!") #prints this if one or more of the letters entered is not an amino acid
@@ -81,13 +55,11 @@ else:
     print("\n", "Length of peptide sequence: ", len(sequence_list), "amino acids")
     print(" Acidic residues: ", acidic_AA/len(sequence_list)*100, "%")
     print(" Basic residues: ", basic_AA/len(sequence_list)*100, "%")
-    print(" Polar residues: ", polar_AA/len(sequence_list)*100, "%")
-    print(" Non-polar residues: ", nonpolar_AA/len(sequence_list)*100, "%")
     print(" Hydrophobic: ", hydrophobic_AA/len(sequence_list)*100, "%")
     print(" Hydrophilic: ", 100-hydrophobicity, "%")
     if hydrophobicity > 50:
-        print(" Your peptide is mostly hydrophobic!")
+        print(" This is a hydrophobic peptide!")
     elif hydrophobicity < 50:
-        print(" Your peptide is mostly hydrophilic!")
+        print(" This is a hydrophilic peptide!")
     else:
-        print(" Your peptide is equally hydrophobic and hydrophilic!")
+        print(" This peptide is equally hydrophobic and hydrophilic!")
